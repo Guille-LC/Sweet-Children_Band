71f932144be80532b068dc1b30444b88c09d0b0e
guillelavi <guillermo96lc@hotmail.com>
2025-02-27 19:20:26 -0300

    js innecesario borrado

--- JavaScript/contacto.js
+++ JavaScript/contacto.js
@@ -1,34 +1,3 @@
-class Miembros {
-    constructor(nombre,apellido,puesto) {
-        this.nombre = nombre,
-        this.apellido = apellido,
-        this.puesto = puesto
-    }
-};
-
-const billie = new Miembros("Billie Joe","Armstrong","Cantante y guitarrista");
-const mike = new Miembros("Mike","Dirnt","Bajista");
-const tre = new Miembros("Tre","Cool","Baterista");
-
-let objetoJason;
-let miembrosDeLaBanda = [];
-
-function transformarJason(obj) {
-    miembrosDeLaBanda.push(obj);
-    objetoJason = JSON.stringify(miembrosDeLaBanda);
-    localStorage.setItem("Nombre y puesto",objetoJason);
-};
-
-transformarJason(billie);
-transformarJason(mike);
-transformarJason(tre);
-
-const arrayJason = JSON.parse(localStorage.getItem("Nombre y puesto"));
-console.log(objetoJason);
-
-
-
-
 let userEmail = document.getElementById("exampleInputEmail1");
 userEmail.onchange = () => {
     localStorage.setItem("correo", userEmail.value);
